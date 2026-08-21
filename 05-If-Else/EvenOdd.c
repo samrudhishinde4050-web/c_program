245d646a6a2cb46c8bd25b5e285ccdf01c761714
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-21 23:33:49 +0530

    EvenOdd.c

---
+++ 05-If-Else/EvenOdd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0)
+    {
+        printf("Number is even");
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+
+    return 0;
+}
