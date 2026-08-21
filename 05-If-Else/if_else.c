6707419810fd251930fc403ef98610303f798e5f
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-21 23:27:10 +0530

    if_else.c

---
+++ 05-If-Else/if_else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int n1;
+  printf("enter the number:");
+  scanf("%d",&n1);
+  if(n1>0)
+  {
+  	printf("n1 is positive");
+  }
+  else{
+  	printf("n1 is negative");
+}
+}
