6ea045195e0880a9fdfef842e96b7feca43c9e52
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-21 23:36:11 +0530

    operators.c

---
+++ 04-Operators/operators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int n1=100,n2=20,n3;
+n3=n1+n2;
+printf("%d+%d=%d \n",n1,n2,n1+n2);
+printf("%d-%d=%d \n",n1,n2,n1-n2);
+printf("%d*%d=%d \n",n1,n2,n1*n2);
+printf("%d/%d=%d \n",n1,n2,n1/n2);
+printf("%d%%%d=%d \n",n1,n2,n1%n2);
+}
