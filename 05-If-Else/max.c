9efd0e365240e841307b9d1c9d3cff4fff5d9199
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-21 23:28:12 +0530

    max number

---
+++ 05-If-Else/max.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int n1,n2,max;
+  printf("enter two number:");
+  scanf("%d%d",&n1,&n2);
+  max=n2;
+  if(n1>n2)
+  max=n1;
+  {
+  	  printf("maximum=%d:", max);
+  }
+
+	}
+	
