ed8b0607d87b7b2d27b5778de29a5b97c691bc51
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-21 23:25:39 +0530

    if.c

---
+++ 05-If-Else/if.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int n1;
+	printf("enter the number of above 15:");
+	scanf("%d",&n1);
+	if(n1<15)
+	{
+		printf("\n You have entered less than 15:");
+	}
+	printf("\n number entered:%d",n1);
+}
