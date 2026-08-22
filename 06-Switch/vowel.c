d427b16ae2c00e5b9281e95d65395cf263e22cce
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-22 23:35:48 +0530

    vowel.c

---
+++ 06-Switch/vowel.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+char ch;
+printf("Enter the the alphabate:");
+scanf("%c",&ch);
+switch(ch)
+{
+	case 'a':printf("vowel");
+	         break;
+
+case 'e':printf("vowel");
+	         break;
+
+case 'i':printf("vowel");
+	         break;
+
+case 'o':printf("vowel");
+	         break;
+
+case 'u':printf("vowel");
+	         break;
+	         default:printf("consanant");
+
+}
+}
