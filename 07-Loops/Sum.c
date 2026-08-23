9898a5b574989160d287e141e3bde1f9a168d1b4
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-23 21:14:31 +0530

    Sum.c

---
+++ 07-Loops/Sum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1, sum = 0;
+
+    while(i <= 5)
+    {
+        sum = sum + i;
+        i++;
+    }
+
+    printf("Sum = %d", sum);
+
+    
+}
+
+
+
+
+
+
+
+
+
