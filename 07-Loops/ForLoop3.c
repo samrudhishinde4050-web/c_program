017bd30333cdd895741e14eb5b56a7c89c3622d6
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-23 21:16:42 +0530

    print even number from 1 to 100 which are divisible by 7

---
+++ 07-Loops/ForLoop3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	
+	//print even number from 1 to 100 which are divisible by 7
+	int i;
+
+	for(i=1;i<=100;i++){
+		if(i%2==0 && i%7==0)
+		printf("%d\n",i);
+	}
+
+
+	 
+}
+
