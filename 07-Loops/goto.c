aa101624cf35b822bbe5512b1a20adb1bb7e59ad
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-28 23:25:34 +0530

    goto.c

---
+++ 07-Loops/goto.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	printf("\n Good");
+	printf("\n Welcome");
+	 
+	 goto ABC;  // goto label
+	 Printf("\n Hii");
+	 printf("\n Hello");
+	 ABC:printf("\n what are you");  // label
+	 printf("\n end");
+}
