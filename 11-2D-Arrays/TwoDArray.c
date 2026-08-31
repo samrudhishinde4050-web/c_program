37b64bed4aeda2f5dda5fab66471d7409edb14e1
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-31 18:05:34 +0530

    TwoDArray.c

---
+++ 11-2D-Arrays/TwoDArray.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+/*	int num[2][3]={10,20,30,40,50,60},i,j;
+	printf("\n The element are:\n");
+	
+	for(i=0;i<2;i++){
+		for(j=0;j<3;j++){
+			printf("%d ",num[i][j]);
+		}
+		printf("\n");
+	}
